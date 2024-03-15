9130a73994bfb2c39801bc7f80a2585d9c6f272e
Genilson fernandes <77983928+geniilsonfernandes@users.noreply.github.com>
2024-03-15 00:28:09 -0300

    feat: inprove documentarion

--- src/components/ui/Input/Input.stories.tsx
+++ src/components/ui/Input/Input.stories.tsx
@@ -33,7 +33,7 @@ export const Compostion: Story = {
   render: (args) => (
     <Input.Root variant={args.variant}>
       <Input.Label htmlFor="name">Nome e sobrenome</Input.Label>
-      <Input {...args} />
+      <Input placeholder='Nome e sobrenome' type='text' id='name' {...args}/>
       <Input.HelperText>ex:Genilson Fernandes</Input.HelperText>
     </Input.Root>
   )
